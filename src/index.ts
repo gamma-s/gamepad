d336da2980cdb39a6d970743a0234115f46a1690
Caner Akdas <canerakdas@gmail.com>
2022-11-21 18:59:37 +0300

    chore: dummy code removed

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
 'use strict';
 
-import Control from './resources/controller/';
+import Gamepad from './resources/controller/';
 
-export default Control;
-
-window.addEventListener('gamepadconnected', event => {
-  const control = new Control(event.gamepad, 'XBOX'); // It would be Control.default when you added directly to the page.
-
-  control.button?.onPress('A', () => {
-    console.log('A');
-  });
-});
+export default Gamepad;
